6985c97b63e6303b4448773c6eb95e8be71b7da8
Lucasdfg <cervantes.lucas99@gmail.com>
2019-11-04 11:22:28 -0600

    added my_any

--- enumerables.rb
+++ enumerables.rb
@@ -30,3 +30,46 @@ module Enumerable
         end
         an_arr
     end
+
+    def my_all?(*arg)
+        if arg.empty?
+            my_each {|i| return false unless yield(i)} if block_given?
+
+            return true if block_given?
+
+            my_each{|i| return false unless i}
+        else
+            my_each { |i| return false unless i.match(arg[0]) } if arg[0].class == Regexp
+
+            return true if arg[0].class == Regexp
+
+            my_each { |i| return false unless i.class == arg[0] } if arg[0].is_a?(Class)
+      return true if arg[0].is_a?(Class)
+
+      my_each { |i| return false unless i == arg[0] }
+    end
+    true
+  end
+
+    def my_any(*arg)
+        if arg.empty?
+            my_each { |i| return true if yield(i) } if block_given?
+      
+            return false if block_given?
+      
+            my_each { |i| return true if i }
+          else
+            my_each { |i| return true if i.match(arg[0]) } if arg[0].class == Regexp
+      
+            return false if arg[0].class == Regexp
+      
+            my_each { |i| return true if i.class == arg[0] } if arg[0].is_a?(Class)
+            return false if arg[0].is_a?(Class)
+      
+            my_each { |i| return true if i == arg[0] }
+          end
+          false
+        end
+      
+
+
