8900b9a15d78c91f42703e141f62193d32914e90
Lucasdfg <cervantes.lucas99@gmail.com>
2019-11-04 10:33:03 -0600

    added my_select

--- enumerables.rb
+++ enumerables.rb
@@ -9,4 +9,24 @@ module Enumerable
             i+=1
         end
     end
-    
+
+    def my_each_with_index
+        return to_enum unless block_given?
+
+        arr = to_a
+        i = 0
+        while i < arr.length
+            yield(arr[i], i)
+            i+=1
+        end
+    end
+
+    def my_select
+        return to_enum unless block_given?
+
+        an_arr = []
+        my_each do|i|
+            an_arr.push(i) if yield(i)
+        end
+        an_arr
+    end
